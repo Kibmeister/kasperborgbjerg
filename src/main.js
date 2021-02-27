d00f1a6d0b8fd99eb7e7d865dbaabfe959c4d42f
Kasper Iverslien Borgbjerg <201808262@post.au.dk>
2021-02-27 14:40:30 +0100

    dependencies removed

--- src/main.js
+++ src/main.js
@@ -13,14 +13,11 @@ import Topico_W from './components/Topico_W.vue'
 import SmartBelt_W from './components/SmartBelt_W.vue'
 import Cleaver_W from './components/Cleaver_W.vue'
 import CoffeeCan_W from './components/CoffeeCan_W.vue'
-import Clippy from './components/Clippy.vue'
 import vueVimeoPlayer from 'vue-vimeo-player'
 import GroupSVG from './assets/svg/pics/topicoDevice.svg';
 import IlluSVG from './assets/svg/pics/illu.svg';
 import ScenarioSVG from './assets/svg/pics/scenario.svg';
-// import CloseSVG from './assets/icons/close.svg'
-// import MaximizeSVG from './assets/icons/maximize.svg'
-// import MinimizeSVG from './assets/icons/minimize.svg'
+
 
 
 Vue.component('Clock', Clock)
@@ -32,15 +29,11 @@ Vue.component('ProjectsWindow', ProjectsWindow)
 Vue.component('ProjectItem', ProjectItem)
 Vue.component('Topico_W', Topico_W)
 Vue.component('SmartBelt_W', SmartBelt_W)
-Vue.component('Clippy', Clippy)
 Vue.component('Cleaver_W', Cleaver_W)
 Vue.component('CoffeeCan_W', CoffeeCan_W)
 Vue.component('GroupSVG', GroupSVG)
 Vue.component('IlluSVG', IlluSVG)
 Vue.component('ScenarioSVG', ScenarioSVG)
-// Vue.component('MaximizeSVG', MaximizeSVG)
-// Vue.component('CloseSVG', CloseSVG)
-// Vue.component('MinimizeSVG', MinimizeSVG)
 
 Vue.use(vueVimeoPlayer)
 window.$ = window.jQuery = require('jquery');
