10e8f74a618811c14f643d6a9fffdf13a42dede6
Kasper Iverslien Borgbjerg <201808262@post.au.dk>
2021-02-27 16:56:41 +0100

    scaled down topico img

--- src/components/SmartBelt_W.vue
+++ src/components/SmartBelt_W.vue
@@ -6,7 +6,7 @@
     v-bind:style="{
       zIndex: zIndex,
 
-      'background-color': contentColor
+      'background-color': contentColor,
     }"
     @mousedown="zIndexToggle"
   >
@@ -21,12 +21,12 @@
     <div class="header">
       <h3>{{ content.title }}</h3>
     </div>
-    <div class="content"> 
+    <div class="content">
       <div class="intro">
         {{ content.intro }}
       </div>
       <h4 id="id_imgTitle">Our awesome belt</h4>
-      <img src="../assets/smartbelt/belt.png" rel="preload" title="source: locally sourced" id="id_img_1" />
+      <img src="../assets/smartbelt/belt.png" title="source: locally sourced" id="id_img_1"/>
       <div class="componentsList">
         <h4>Components list</h4>
         <ul>
@@ -58,63 +58,79 @@
         ></vimeo-player>
       </div>
       <div class="videoDescription">
-        This demo is proof that our project is doable, designing and developing a self adjusting
-        belt. And keep in mind, this is a proof of concept project, no commercial thoughts behind.
+        This demo is proof that our project is doable, designing and developing
+        a self adjusting belt. And keep in mind, this is a proof of concept
+        project, no commercial thoughts behind.
       </div>
 
-      
       <div class="prototypeDesign first">
         <h5>What we have buildt</h5>
-        The prototype consist of a bigger main box that serves as the belt buckle, housing all of
-        the electronics, except from the mobile battery that supplies the circuit. From the outside,
-        the top surface holds the button for switching the direction of the stepper motor. This
-        capability is necessary for the design, as it functions as our locking mechanism. we
-        discussed different mechanisms for locking the belt in, but landed on the one mentioned
-        above as it was the simplest solution that demanded the least space. On each side of the
-        button you find two LED’s, one red and one green, indicating if the belt spools in or out.
+        The prototype consist of a bigger main box that serves as the belt
+        buckle, housing all of the electronics, except from the mobile battery
+        that supplies the circuit. From the outside, the top surface holds the
+        button for switching the direction of the stepper motor. This capability
+        is necessary for the design, as it functions as our locking mechanism.
+        we discussed different mechanisms for locking the belt in, but landed on
+        the one mentioned above as it was the simplest solution that demanded
+        the least space. On each side of the button you find two LED’s, one red
+        and one green, indicating if the belt spools in or out.
       </div>
 
       <div class="prototypeDesign second">
-        On the side of the box facing outwards, you find the force sensitive resistor (FSR), that
-        functions as the activator for the motor inside the belt buckle. The analogue values from
-        the sensor is mapped to two different speeds, that turns the motor. The user then has the
-        possibility of running the motor in two different speeds as it might me suitable for the
-        situation at hand. Inside of the buckle, there are three hardware components, the PCB with
-        the microchip mounted on, the stepper motor itself, and the driver board for the motor.
+        On the side of the box facing outwards, you find the force sensitive
+        resistor (FSR), that functions as the activator for the motor inside the
+        belt buckle. The analogue values from the sensor is mapped to two
+        different speeds, that turns the motor. The user then has the
+        possibility of running the motor in two different speeds as it might me
+        suitable for the situation at hand. Inside of the buckle, there are
+        three hardware components, the PCB with the microchip mounted on, the
+        stepper motor itself, and the driver board for the motor.
       </div>
 
       <div class="prototypeDesign third">
-        The motor is tightly secured by two opposing walls, as well as a notched wall on the top,
-        hindering it in moving vertical. The main gear is mounted on the stepper motors axle,
-        vertically, and held securely by a wheel baring on the opposing side, enabling steady
-        rotation of the gear. Parallel to the main gear, there is a second gear mounted to two wheel
-        barrings, one below and one above the gear. The two gears have a similar additional outer
-        gear on their top side, which interconnects, making the second gears rotation fully
-        dependent on the main gears rotation.
+        The motor is tightly secured by two opposing walls, as well as a notched
+        wall on the top, hindering it in moving vertical. The main gear is
+        mounted on the stepper motors axle, vertically, and held securely by a
+        wheel baring on the opposing side, enabling steady rotation of the gear.
+        Parallel to the main gear, there is a second gear mounted to two wheel
+        barrings, one below and one above the gear. The two gears have a similar
+        additional outer gear on their top side, which interconnects, making the
+        second gears rotation fully dependent on the main gears rotation.
       </div>
 
       <div class="prototypeDesign fourth">
-        Between the two gears there is a 2mm gap, where the belt itself is feed through. The gap is
-        tighter than the belt itself, and therefore grabs the belt as it is feed through the opening
-        and spools is further in. This leads to the two cutouts for the belt to go through, on the
-        left and right side of the belt buckle. These cutouts are aligned, so the belt is spooled
-        through on a parallel path as where the two gears are mounted on the inside of the box.
+        Between the two gears there is a 2mm gap, where the belt itself is feed
+        through. The gap is tighter than the belt itself, and therefore grabs
+        the belt as it is feed through the opening and spools is further in.
+        This leads to the two cutouts for the belt to go through, on the left
+        and right side of the belt buckle. These cutouts are aligned, so the
+        belt is spooled through on a parallel path as where the two gears are
+        mounted on the inside of the box.
       </div>
 
       <div class="prototypeDesign fifth">
-        On the inside of the box, there is a second compartment, located in between the outer wall
-        and a partition wall north of the main gear. This compartment is reserved for housing the
-        PCB board and the driver board. At last, there is a cutout on the left upper corner of the
-        wall, where the start tip of the belt is mounted on.
+        On the inside of the box, there is a second compartment, located in
+        between the outer wall and a partition wall north of the main gear. This
+        compartment is reserved for housing the PCB board and the driver board.
+        At last, there is a cutout on the left upper corner of the wall, where
+        the start tip of the belt is mounted on.
       </div>
       <div class="prototypeDesign sixth">
-        <h5>PCB backside</h5> 
-        <img src="../assets/smartbelt/pcbBack.png" rel="preload" id="id_backSideIMG" />
+        <h5>PCB backside</h5>
+        <img
+          src="../assets/smartbelt/pcbBack.png"
+          rel="preload"
+          id="id_backSideIMG"
+        />
       </div>
 
       <div class="exploaded">
         <h5>Exploaded 3D model</h5>
-        <img src="../assets/smartbelt/exploaded.png" rel="preload" id="id_exploadedIMG" />
+        <img
+          src="../assets/smartbelt/exploaded.png"
+          rel="preload"
+          id="id_exploadedIMG"
+        />
       </div>
       <div class="pcb">
         <h5>PCB with components</h5>
@@ -142,9 +158,9 @@ export default {
     id: String,
     contentColor: String,
     width: Number,
-    height: Number
+    height: Number,
   },
-  mixins: [windowScript]
+  mixins: [windowScript],
 };
 </script>
 
@@ -209,7 +225,6 @@ export default {
     place-self: center start;
     grid-column: 2/3;
     grid-row: 4/5;
-    
   }
   .prototypeDesign.first {
     grid-column: 3/4;
@@ -218,7 +233,6 @@ export default {
       margin-bottom: 10px;
       margin-top: 0;
     }
-
   }
   .prototypeDesign.second {
     grid-column: 4/5;
@@ -244,7 +258,7 @@ export default {
     h5 {
       text-align: center;
       margin-top: 0;
-    }     
+    }
   }
 
   .exploaded {
